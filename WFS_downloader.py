1e0276f4292a510bcff33147b1aaaa88db36251d
Saulocmf <98889062+Saulocmf@users.noreply.github.com>
2023-09-14 16:22:17 +0200

    Update WFS_downloader.py

--- WFS_downloader.py
+++ WFS_downloader.py
@@ -4,7 +4,7 @@ import xml.dom.minidom
     
 base_url = "https://inspire-hessen.de/ows/services/org.2.e305d5c1-54e3-4ef4-b0a1-527237ac7bfa_wfs?"
 layer_name = "cp:CadastralParcel"
-output_path = r"C:\DVP_Projects\data\_wfsAllPages.gml"
+output_path = r"C:\....gml"
 
 # Get the max batch size for each page
 request_size = requests.get(base_url , params={
@@ -17,7 +17,7 @@ for element in constraint_elements :
     if element.getAttribute("name") == "CountDefault":
         default_value = element.getElementsByTagName("ows:DefaultValue")[0]
         batch_size = int(default_value.firstChild.nodeValue)
-        
+
 # Calculate the number of pages for the resquest:
 request_hits = requests.get(base_url , params={
     "service": "WFS",
@@ -27,6 +27,7 @@ request_hits = requests.get(base_url , params={
     "outputFormat": "application/gml+xml; version=3.2",
     "resultType":"hits"
 })
+print(request_hits.text)
 result_parse = xml.dom.minidom.parseString(request_hits.content)
 result_fc = result_parse.getElementsByTagName("wfs:FeatureCollection")
 total_hits = int(result_fc[0].getAttribute("numberOfFeatures"))
@@ -41,6 +42,7 @@ params1 = {
     "version": "1.1.0",
     "request": "GetFeature",
     "typeName": "cp:CadastralParcel",
+    "maxFeatures":"1",
     "outputFormat": "application/gml+xml; version=3.2"
 }
 
@@ -51,9 +53,9 @@ header = response_header.text.split("<gml:featureMember>")[0]
 output_file.write(header.encode('utf-8'))
 
 # Loop through the pages and fetch only the elements that represent the actual features (exclude each page header)
+# for page_number in tqdm(range(total_pages), desc ="Going through pages"):
 start_index = 0
-for page_number in tqdm(range(total_pages), desc ="Going through pages"):
-    ## - to deletion - start_index = page_number * batch_size
+while start_index < total_hits :
     params2 = {
         "service": "WFS",
         "version": "1.1.0",
@@ -61,12 +63,11 @@ for page_number in tqdm(range(total_pages), desc ="Going through pages"):
         "typeName": layer_name,
         "startIndex": start_index,
         "maxFeatures": batch_size,
-        "outputFormat": "application/gml+xml; version=3.2"
-    }
+        "outputFormat": "application/gml+xml; version=3.2",
+}
     response = requests.get(base_url, params=params2, stream=True)
     root = xml.dom.minidom.parseString(response.content)
-    start_index += batch_size +1
-    
+    start_index += batch_size
     # Find all featureCollection
     feature_collections = root.getElementsByTagName("gml:FeatureCollection")
     for feature_collection in feature_collections:
@@ -76,7 +77,7 @@ for page_number in tqdm(range(total_pages), desc ="Going through pages"):
         for feature_member in feature_members:
             output_file.write(feature_member.toxml().encode('utf-8')) # write the element in the outputfile
             #print(feature_member.toxml(), file=output_file)
-                
+            
 # Close the GML file with the closing tag
 closing_gml = "\n</gml:FeatureCollection>"
 output_file.write(closing_gml.encode('utf-8'))
